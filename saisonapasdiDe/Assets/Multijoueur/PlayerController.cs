1e43e76cb9b1dfb778927fc5da93739c953fcd5d
ludovic maitre <ludovic.maitre@epita.fr>
2020-01-27 19:43:03 +0100

    nouveau saisonapasdide

---
+++ saisonapasdiDe/Assets/Multijoueur/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+
+
+public class PlayerController : NetworkBehaviour
+{ 
+    void Update()
+    {
+        if (!isLocalPlayer) //code de déplacement + vérification pr voir si le joueur est un joueur local 
+        {
+            return;
+        }
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0,x,0);
+        transform.Translate(0,0, z);
+    }
+
+    public override void OnStartLocalPlayer() //pour avoir la couleur des joueurs différents
+    {
+        GetComponent<MeshRenderer>().material.color = Color.blue;
+    }
+}
---
+++ saisonapasdiDe/Assets/Personnage/armes/ShotScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotScript : MonoBehaviour
+{
+    public int speed = 3; // vitesse du projectile en seconde.
+    public int damage = 1; // point de dégat
+    
+    public bool isEnemyShot = false; //projetcile enemie ou alors ami
+// Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 20); // 20 seconde.
+    }
+
+// Update is called once per fram
+}

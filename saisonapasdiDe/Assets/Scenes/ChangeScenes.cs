4140c8a421de1c4748335cfb1bf003f18bb25dbf
ludovic maitre <ludovic.maitre@epita.fr>
2020-01-30 21:44:28 +0100

    animation

---
+++ saisonapasdiDe/Assets/Scenes/ChangeScenes.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class ChangeScenes : MonoBehaviour {
+    public string LevelToLoad;
+    void LoadMenu () {
+        SceneManager.LoadScene("maptestobj");
+    }
+    void LoadLevel()
+    {
+        SceneManager.LoadScene(LevelToLoad);
+    }
+}

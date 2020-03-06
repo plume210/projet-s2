cb206ba54742f61bfff50daa191d5369381dcdf6
ludovic maitre <ludovic.maitre@epita.fr>
2020-03-06 13:39:47 +0100

    multi fonctionnel

--- saisonapasdiDe/Assets/Personnage/pinguin/PlayerController.cs
+++ saisonapasdiDe/Assets/Personnage/pinguin/PlayerController.cs
@@ -18,20 +18,22 @@ public class PlayerController : NetworkBehaviour
     {
         if (!isLocalPlayer) //code de déplacement + vérification pr voir si le joueur est un joueur local 
         {
-            var x1 = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-            var z1 = Input.GetAxis("Vertical") * Time.deltaTime * 150.0f;
-            var y1 = Input.GetAxis("Jump");
-            var angle = Input.GetAxisRaw("Mouse Y")*Time.deltaTime*speed;
-            var angle2 = Input.GetAxisRaw("Mouse X")*Time.deltaTime*speed;
-            transform.Translate(z1, 0, x1);
-            var positiondujoueur = new Vector3(x1,y1);
-           // transform.Rotate(positiondujoueur, angle2);
+            return;
         }
+        
+        var x1 = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z1 = Input.GetAxis("Vertical") * Time.deltaTime * 150.0f;
+        var y1 = Input.GetAxis("Jump");
+        //var angle = Input.GetAxisRaw("Mouse Y")*Time.deltaTime*speed;
+        //var angle2 = Input.GetAxisRaw("Mouse X")*Time.deltaTime*speed;
+        transform.Translate(z1, 0, x1);
+        //var positiondujoueur = new Vector3(x1,y1);
+        // transform.Rotate(positiondujoueur, angle2);
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             Fire();
         }
-
+        
         void Fire()
         {
             // instantiate = creer a une certaine position
@@ -39,12 +41,12 @@ public class PlayerController : NetworkBehaviour
             // ajout de la velocite a la balle
             bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletspeed;
             // f = temps (2seconde)
-           // Destroy(bullet,2.0f);
+            // Destroy(bullet,2.0f);
         }
         
     }
     public override void OnStartLocalPlayer() //pour avoir la couleur des joueurs différents
-        {
-            GetComponent<MeshRenderer>().material.color = Color.blue; //a changer (1 joueur = 1 skins choisit au prealable)
-        }
+    {
+        GetComponent<MeshRenderer>().material.color = Color.blue; //a changer (1 joueur = 1 skins choisit au prealable)
+    }
 }

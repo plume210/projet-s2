ae961183e1741917e6c462343765e5386511e28c
ludovic maitre <ludovic.maitre@epita.fr>
2020-03-16 11:50:20 +0100

    jeu

---
+++ ROGUE/saisonapasdiDe/Assets/Photon/PhotonUnityNetworking/Resources/Personnage/pinguin/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Runtime.ExceptionServices;
+using Photon.Pun;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+
+{
+    public GameObject balled;
+    public Transform bulletspawn;
+    public float bulletspeed = 300.0f;
+    public float speed = 300.0f;
+    private PhotonView _view;
+    private Camera _camera;
+
+    private void Start()
+    {
+        _view = GetComponent<PhotonView>();
+        _camera = GetComponentInChildren<Camera>();
+    }
+
+    void Update()
+    {
+        if (_view.IsMine)
+        {
+            
+            var x1 = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+            var z1 = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+            transform.Rotate(0, x1, 0);
+            transform.Translate(0, 0, z1);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                var bullet = PhotonNetwork.Instantiate(balled.name, bulletspawn.position, bulletspawn.rotation,0);
+                bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletspeed;
+            }
+        }
+    }
+}
---
+++ ROGUE/saisonapasdiDe/Assets/joinscript.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+using System.Collections;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class joinscript : MonoBehaviourPunCallbacks
+{
+    public Transform spawn;
+    public GameObject player;
+    public Camera camera;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+        PhotonNetwork.ConnectUsingSettings();
+        Debug.Log("connected to " + PhotonNetwork.CloudRegion);
+    }
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+        Debug.Log("connected to " + PhotonNetwork.CurrentLobby);
+    }
+
+    public override void OnJoinedLobby()
+    {
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 2;
+        PhotonNetwork.JoinOrCreateRoom("Room", roomOptions, TypedLobby.Default);
+        Debug.Log("Creating room: " + PhotonNetwork.CurrentRoom);
+        //PhotonNetwork.JoinRoom("Room");
+        Debug.Log("Joined " + PhotonNetwork.CurrentRoom);
+    }
+
+    public override void OnJoinedRoom()
+    {
+
+        StartCoroutine(Spawnplayer());
+        Debug.Log("Dématérialisation !!!!!!");
+    }
+
+    IEnumerator Spawnplayer()
+    {
+        GameObject Myplayer = PhotonNetwork.Instantiate(player.name,spawn.position, Quaternion.identity, 0);
+        camera.GetComponent<CameraSmooth>().target = Myplayer.transform;
+        yield return new WaitForSeconds(5);
+        Myplayer.AddComponent<Rigidbody>();
+        Myplayer.AddComponent<Rigidbody>().collisionDetectionMode = CollisionDetectionMode.Continuous;
+    }
+}

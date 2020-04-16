092afb961f2e18b164f3d6cc53356e54c116c204
Alois Cavailles <alois.cavailles-wurmser@epita.fr>
2020-04-16 22:47:11 +0200

    tes

---
+++ ROGUE/Teest/Assets/ScriptPlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Runtime.ExceptionServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScriptPlayerController : MonoBehaviour
+{
+    public Animator anim;
+    public float speed = 300.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var x1 = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        var z1 = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        anim.SetFloat("vertical", Input.GetAxis("Vertical"));
+        anim.SetFloat("horizontal", Input.GetAxis("Horizontal"));
+
+        transform.Rotate(0, 0, x1);
+        transform.Translate(-z1, 0, 0);
+    }
+}
--- ROGUE/saisonapasdiDe/Assets/Photon/PhotonUnityNetworking/Resources/Personnage/pinguin/PlayerController.cs
+++ ROGUE/saisonapasdiDe/Assets/Photon/PhotonUnityNetworking/Resources/Personnage/pinguin/PlayerController.cs
@@ -11,6 +11,8 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 
 {
+    private Animator anim;
+
     public GameObject balled;
     public Transform bulletspawn;
     public float bulletspeed = 300.0f;
@@ -22,17 +24,26 @@ public class PlayerController : MonoBehaviour
     {
         _view = GetComponent<PhotonView>();
         _camera = GetComponentInChildren<Camera>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
         if (_view.IsMine)
         {
-            
+            //prendre les imputs
             var x1 = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
             var z1 = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+            //anim
+            anim.SetFloat("vertical", z1);
+      
+
+            //deplacement
             transform.Rotate(0, x1, 0);
             transform.Translate(0, 0, z1);
+
+            //tir
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 var bullet = PhotonNetwork.Instantiate(balled.name, bulletspawn.position, bulletspawn.rotation,0);
